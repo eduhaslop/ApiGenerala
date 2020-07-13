93e2f65893c68f73a855d8af47dc6b934a7dd59c
bauti <bautiquere@gmail.com>
2020-07-13 11:43:58 -0300

    agregar login y registro

---
+++ routes/login.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const dataUsuarios = require('../data/usuario');
+const router = express.Router();
+
+router.get('/', async (req, res)=>{
+    let usuario = await dataUsuarios.getUsuarioWithMail(req.body.mail);
+
+    if (!!usuario && usuario.pass === req.body.pass) {
+        res.send(usuario);
+    } else {
+        req.status(401).send('email o contrasenia incorrectos');
+    }
+});
+
+
---
+++ routes/register.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const dataUsuarios = require('../data/usuario');
+const router = express.Router();
+
+
+router.post('/', async (req, res)=>{
+    let previousUser = await dataUsuarios.getUsuarioWithMail(req.body.mail);
+   
+    if (!!previousUser) {
+        res.status(409).send('El usuario ya esta registrado');
+    } else {
+        let result = await dataUsuarios.pushUsuario(
+            {
+                nombre: req.body.nombre,
+                mail: req.body.mail,
+                pass: req.body.pass,
+                victorias: 0,
+                derrotas: 0,
+                score: 0,
+                record: 0,
+                promedio: 0
+            }
+        );
+    
+        res.send(result)
+    }
+});
